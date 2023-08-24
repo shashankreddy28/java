1b0b870016107eda5ea89651651c120cce66e1bb
Shash Reddy <shashreddy@Shashs-Air.lan>
2023-08-24 00:34:50 -0500

    making changes in all files except Main.java

--- CIS122.java
+++ CIS122.java
@@ -3,5 +3,6 @@
 public class CIS122 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(true);// true is boolean (not True like python)
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java
 using this repo to update all the code I write in CIS122 class
+
+## Checking subheader
