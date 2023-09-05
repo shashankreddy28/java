b082953d6b5d5e639f3a7f9ec1e1b6e006570a68
shashankreddy28 <122624735+shashankreddy28@users.noreply.github.com>
2023-09-05 01:20:01 -0500

    Delete CIS122.java
    deleting all files

--- CIS122.java
+++
@@ -1,8 +0,0 @@
-
-
-public class CIS122 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(true);// true is boolean (not True like python)
-    }
-}
