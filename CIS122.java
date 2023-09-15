29671023a21cbed9ec2c85808a7f2f70776ea72a
shashankreddy28 <qn1833ep@go.minnstate.edu>
2023-09-15 00:21:24 -0500

    added lots of new code, testing individual parts

--- CIS122.java
+++ CIS122.java
@@ -1,39 +1,112 @@
-//  import java.util.Scanner;
+ import java.util.Scanner;
 
 public class CIS122 {
     public static void main(String[] args) {
+        //Scanner in = new Scanner(System.in);
         System.out.println("Hello World!");
+        double mydouble = 10.1;
+        if(mydouble%10==(0)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("no");
+        }
+
         double c= 4.951;
         double bottle =1.949;
         System.out.println(c+bottle);//doesnt give expected answer due to roundoff error
         double vdouble =10;// automatically creates it into 10.0
+
         int vint =10;
         System.out.println(Math.pow(10, 3)); //looks like it always has decimal point with pow fnc
         System.out.println(Math.pow(vdouble,3));
         System.out.println(Math.pow(vint, 3));
+
         System.out.println(Math.abs(3-10));//no decimal place
+
         System.out.println(vdouble/vint);// give a double output
+
         System.out.println("Hey\nhello");
         System.out.printf("Hey%nhello\n");
+
         int new_int = -14510;
         System.out.println(new_int%100); //generally used to get the last 2 digits, but if the result is 0 then the last 2 digits are 00. when the number >= 0 otherwise, it will give negative of the last 2 digits so, to use it we will have to use abdsolte value of it
+        
         double variable = 185.92 ;
         long roundedVariable = Math.round(variable);//round always returns long value if we want to use it as int, we will have to convert it.
         int roundeedInt = (int) roundedVariable;//converting the round to int type
+        
         System.out.println(roundedVariable +"   " +roundeedInt);
-        int CIS122 = 40;
+        int CIS122 = 40;//file name can be used as a variable
         System.out.println(CIS122);
 
-        //avoiding round off errors:
-        double r = Math.sqrt(2);
-        double delta = (r*r) - 2;
-        double EPSILON = 1E-14;
-        if (delta <= EPSILON ){
-            System.out.println("sqrt(2) squared minus 2 is 0");
+        // avoiding round off errors:
+            double r = Math.sqrt(2);
+            double delta = (r*r) - 2;
+            double EPSILON = 1E-14;
+            if (delta <= EPSILON ){
+                System.out.println("sqrt(2) squared minus 2 is 0");
+            }
+            else {
+                System.out.println("sqrt(2) squared minus 2 is " + delta);
         }
-        else {
-            System.out.println("sqrt(2) squared minus 2 is " + delta);
+        
+        
+        ///*calculating volume of cube + volume of sphere */
+        // double s = 1;
+        // double d = 2;
+        // double r = d/2;
+        // double sq = Math.pow(s,3);
+        // double sp = (4.0/3) * Math.PI * (Math.pow(r,3));
+
+        // System.out.println(sq + sp);
+        // int inputnum = in.nextInt();
+        // System.out.println(inputnum);
+        
+        ////checking for pythogorian triplet:
+        // int a = 2;
+        // int b = 2;
+        // int c = 4;
+        // int aSquare = a*a;
+        // int bSquare = b*b;
+        // int cSquare = c*c;
+        // double aSquare = Math.pow((double)(a),(double)(2));
+        // double bSquare = Math.pow((double)(b),(double)(2));
+        // double cSquare = Math.pow((double)(c),(double)(2));
+        // boolean test = (cSquare == (aSquare+bSquare));
+        // if (a>=b && a>=c){
+        //     test = (aSquare == (cSquare+bSquare));
+        // }
+        // else if (b>=a && b>=c){
+        //     test = (bSquare == (aSquare+cSquare));
+        // }
+        // System.out.println(test);
+
+        boolean isint = false;
+        System.out.println("enter homeruns:");
+        // int hr =0;
+        while (isint ==false){
+            Scanner in = new Scanner(System.in);
+            
+            if(in.hasNextInt()){
+                int hr = in.nextInt();
+                System.out.println(hr);
+                isint = true;
+
+            }
+            else{
+                System.out.println("Enter integer:");
+                
+                
+            }
         }
+        
+
+
+
+
+
+
     }
 }
 
