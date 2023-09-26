0b8603a4892acb08a10711d56aac5c82029e22e4
shashankreddy28 <qn1833ep@go.minnstate.edu>
2023-09-26 01:53:20 -0500

    removed comments in game.java file

--- game.java
+++ game.java
@@ -1,14 +1,6 @@
 import java.util.Scanner;
 public class game {
-        // static String one = "N";
-        // static String two = "N";
-        // static String three = "N";
-        // static String four = "N";
-        // static String five = "N";
-        // static String six = "N";
-        // static String seven = "N";
-        // static String eight = "N";
-        // static String nine = "N";
+
     public static void createStructure(char[] gameList){
             System.out.printf(" %c | %c | %c \n",gameList[0] , gameList[1] , gameList[2]);
             System.out.println("--- --- ---");
@@ -20,20 +12,11 @@ public class game {
         boolean gameOver =false;
         Scanner in = new Scanner(System.in);
         
-        System.out.println("Tic tac toe");
+        System.out.println("\nTic tac toe\n");
         int xCount = 0;
         int[] x = new int[5];
-        // x[0] = 1;
-        // x[1] = 3;
-        // x[2] = 5;
-        // x[3] = 7;
-        // x[4] = 9;
         int oCount = 0;
         int[] o = new int[4];
-        // o[0] = 2;
-        // o[1] = 4;
-        // o[2] = 6;
-        // o[3] = 8;
         char[] gameList = {'N','N','N','N','N','N','N','N','N'};
         for(int i=0;i<4;i++){
             System.out.println("Player 'x' enter number bw 1 & 9:");
@@ -42,7 +25,6 @@ public class game {
                 x[xCount]=xInput;
                 xCount++;
                 for(int element :x){
-                    //System.out.println(element);
                     if (element>=1 && element <=9){gameList[element-1]='X';}
                 }
                 createStructure(gameList);
@@ -60,18 +42,7 @@ public class game {
                 }
             }
         }
-        // for(int element :x){
-        //     //System.out.println(element);
-        //     if (element>=1 && element <=9){gameList[element-1]='X';}
-        // }
-        // for(char element :gameList){
-        //     System.out.println(element);
-        // }
-        // for(int element :o){
-        //     if (element>=1 && element <=9){gameList[element-1]='O';}
-        // }
         in.close();
-        //createStructure(gameList);
     }
     
 }
