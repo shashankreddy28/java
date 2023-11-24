9cbc873c457ed9663e114a6d8b3da7ffb5ac50c3
shashankreddy28 <qn1833ep@go.minnstate.edu>
2023-11-24 14:06:29 -0600

    tested in-built linked list in java

---
+++ LinkedList.java
@@ -0,0 +1,7 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+public class LinkedListTest{
+    public static void main(String[] args){
+
+    }
+}
---
+++ LinkedListTest.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Iterator;
+public class LinkedListTest{
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Hi");
+        list.add("Hello");
+        list.add("Bro");
+        ListIterator<String> iter = list.listIterator();
+        while(iter.hasNext()){
+            System.out.print(iter.next() + " ");
+            iter.remove(); // removes the element the iterator just went over
+            iter.add("and"); // adds element right before the iterator
+        }
+        System.out.println();
+        System.out.println(list.contains("Bro"));
+        for(String element: list){
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        if(iter.hasPrevious()){iter.previous();iter.add("new word");} // we can see that .add() only adds element right before the iterator position 
+        for(String element: list){
+            System.out.print(element + " ");
+        }
+    }
+}
