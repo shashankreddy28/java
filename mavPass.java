d2664a978cc24bd42ff9ee96a5f3aac0985a0aca
shashankreddy28 <qn1833ep@go.minnstate.edu>
2023-12-03 21:06:28 -0600

    practice for final exam, in teh mavPass.java file and mavPass2.java file

---
+++ mavPass.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class mavPass {
+    public static void main(String[] args){
+        System.out.println("hello World");
+        int[][] arr = {{1,2,3},
+                        {4,5,6},
+                        {7,8,9}};
+                        int[][] arr2 = {{1,1,1},
+                        {1,1,1},
+                        {1,1,1}};
+        print2DArr(arr);
+        System.out.println();
+        swap(arr);
+        print2DArr(arr);
+        System.out.println(identicalTest(arr2)); //true
+        System.out.println(identicalTest(arr));  //false  
+        System.out.println(summ(2)); 
+        System.out.println(powerN(5,3));
+    }
+    public static void print2DArr(int[][] arr){
+        for(int i=0;i<arr.length;i++){for(int j = 0; j < arr[i].length;j++){System.out.print(arr[i][j]+" , ");}System.out.println();}
+
+    }
+    public static void swap(int[][] arr){ // swaps first and secod row in a 2d array
+        int[] newArr;
+        newArr = Arrays.copyOf(arr[0],arr[0].length);
+        arr[0] = arr[1];
+        arr[1] = newArr;
+    }
+    public static boolean identicalTest(int[][] arr){//checks if the array has all same elements.
+        int first = arr[0][0];
+        for(int i =0; i<arr.length;i++){
+            for(int j = 0; j<arr[i].length;j++){
+                if(arr[i][j]!=first){return false;}
+            }
+        }
+        return true;
+    }
+    public static int summ(int x){
+        if(x<=1){return 1;}
+        return x+summ(x-1);
+    }
+    public static int powerN(int base,int exponent){
+        if(exponent<=0){return 1;}
+        else{
+            return base * powerN(base, exponent-1);
+        }
+    }
+}
---
+++ mavPass2.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class mavPass2 {
+    public static void main(String[] args){
+        System.out.println();
+        Rect rect1 = new Rect(1,1);
+        Rect rect2 = new Rect(2,2);
+        Rect rect3 = new Rect(3,3);
+        ArrayList<Rect> arr = new ArrayList<>();
+        arr.add(rect3);
+        arr.add(rect2);
+        arr.add(rect1);
+        for(Rect i : arr){System.out.println(i);}
+        Collections.sort(arr);
+        System.out.println();
+        for(Rect i : arr){System.out.println(i);}       
+    }
+}
+
+interface Shape{
+    public int calcArea();
+    public int calcPeri();
+}
+class Rect implements Shape , Comparable{
+        private int length;
+        private int breath;
+        public Rect(){
+            length =0;
+            breath =0;
+        }
+        public Rect(int l, int b){
+            length =l;
+            breath =b;
+        }
+        public int calcArea(){
+            return length * breath;
+        }
+        public int calcPeri(){
+            return 2*(length+breath);
+        }
+        public int compareTo(Object obj){
+            Rect obj1 = (Rect)(obj);
+            return this.calcArea()-obj1.calcArea();
+        }
+        // public boolean equals(Object rect){
+        //     Rect rect1 = (Rect)(rect);
+        //     return this.calcArea() == rect1.calcArea();
+        // }
+        public String toString(){
+            return "rect has area : "+calcArea()+" .\n";
+        }
+
+
+}
+class Circle implements Shape{
+    private int radius;
+    public Circle(){
+        radius =0;
+    }
+    public Circle(int r){
+        radius = r;
+    }
+    public int calcArea(){
+        return 3*radius*radius;
+    }
+    public int calcPeri(){
+        return 2*3*radius;
+    }
+
+
+}
